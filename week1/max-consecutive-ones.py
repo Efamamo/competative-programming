eba0f5acde9cfba0d7f110fe2c1077da97fe0792
Efamamo <144004369+Efamamo@users.noreply.github.com>
2023-12-04 05:37:41 -0900

    Added solution for Max Consecutive Ones

---
+++ week1/max-consecutive-ones.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        max_count=0
+        count=0
+        for r in range(len(nums)):
+            if nums[r]==1:
+                count+=1
+                max_count=max(max_count,count)
+            else:
+                count=0
+        return max_count
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
