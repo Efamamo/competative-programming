7732c8b694cc281631957caab6ea7a70272176a6
Efamamo <144004369+Efamamo@users.noreply.github.com>
2024-01-02 19:02:28 +0300

    Added solution for Move Zeroes

---
+++ week1/move-zeroes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        i=0
+        j=0
+        while i<len(nums):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+                
+            i+=1
+           
+      
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
